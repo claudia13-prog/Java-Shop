d81c15c5a489bb7890408df3a2cc1b73bb7eecf1
claudia13-prog <cappellaclaudia13@gmail.com>
2024-11-20 18:26:21 +0100

    added constructor

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -9,4 +9,15 @@ public class Prodotto {
     float prezzo;
     float iva;
 
+    // Metodi
+
+    // Costruttore
+    public Prodotto(int codice, String nome, String descrizione, float prezzo, float iva){
+        this.codice = codice;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
 }
