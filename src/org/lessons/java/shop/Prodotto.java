6a1739d40ca9a5ff0b7252b1bbada4dba787dc14
claudia13-prog <cappellaclaudia13@gmail.com>
2024-11-20 18:03:02 +0100

    assigned instance variables to the Prodotto class

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -2,4 +2,11 @@ package org.lessons.java.shop;
 
 public class Prodotto {
 
+    // Variabili d'istanza
+    int codice;
+    String nome;
+    String descrizione;
+    float prezzo;
+    float iva;
+
 }
